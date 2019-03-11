bd9eb9ead9930136accafd1f1d4f53dbb1bec507
fernando <fernando@fernandos-MacBook-Air.local>
2019-03-11 09:06:48 -0600

    first commit

---
+++ mapa/CustomAnnotation.h
@@ -0,0 +1,17 @@
+//
+//  CustomAnnotation.h
+//  mapa
+//
+//  Created by fernando on 3/11/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CustomAnnotation : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ mapa/CustomAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  CustomAnnotation.m
+//  mapa
+//
+//  Created by fernando on 3/11/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+#import "CustomAnnotation.h"
+
+@implementation CustomAnnotation
+
+@end
